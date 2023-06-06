f16df07b84cc85c6597705bbf3557b7d83b86873
aryawijaya508 <aryawijaya508@gmail.com>
2023-06-06 12:10:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import navbar from './components/navbar.vue';
+import hero from './components/hero.vue'
+</script>
+
+<template>
+  <header>
+    <navbar/>
+  </header>
+
+  <main>
+    <hero/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
